feb272bbadc1a426ae5bf4216f85acb03036c732
SuBroXz11 <subratkrp2@gmail.com>
2026-05-17 12:06:00 +0545

    feat: re-add Create your own Survey button from ending card

--- apps/web/app/lib/survey-builder.ts
+++ apps/web/app/lib/survey-builder.ts
@@ -83,6 +83,8 @@ export const getDefaultEndingCard = (languages: TSurveyLanguage[], t: TFunction)
     type: "endScreen",
     headline: createI18nString(t("templates.default_ending_card_headline"), languageCodes),
     subheader: createI18nString(t("templates.default_ending_card_subheader"), languageCodes),
+    buttonLabel: createI18nString(t("templates.default_ending_card_button_label"), languageCodes),
+    buttonLink: "https://formbricks.com",
   };
 };
 

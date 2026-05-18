86a8e151f7755f34ba8ae045d1cd4f6e9098dd01
SuBroXz11 <subratkrp2@gmail.com>
2026-05-18 13:06:32 +0545

    fix: remove powered-by-formbricks logo from survey loading animation

--- packages/surveys/src/components/general/survey.tsx
+++ packages/surveys/src/components/general/survey.tsx
@@ -14,7 +14,6 @@ import { TSurveyElement } from "@formbricks/types/surveys/elements";
 import { BlockConditional } from "@/components/general/block-conditional";
 import { EndingCard } from "@/components/general/ending-card";
 import { ErrorComponent } from "@/components/general/error-component";
-import { FormbricksBranding } from "@/components/general/formbricks-branding";
 import { LanguageSwitch } from "@/components/general/language-switch";
 import { ProgressBar } from "@/components/general/progress-bar";
 import { RecaptchaBranding } from "@/components/general/recaptcha-branding";
@@ -77,7 +76,6 @@ export function Survey({
   mode,
   survey,
   styling,
-  isBrandingEnabled,
   onDisplay,
   onResponse,
   onClose,
